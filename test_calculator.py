fa13a7a238b8b7da1824cc6f844c8f74fe3643a2
Eduard0M0nteir0 <edu.alvaro.monteiro@gmail.com>
2024-10-26 15:35:29 -0300

    Changes to test function naming

--- test_calculator.py
+++ test_calculator.py
@@ -2,32 +2,32 @@ from calculator import Calculator
 
 calculator = Calculator()
 
-def sum_test():
+def test_sum():
     assert calculator.calculate(1, 2, '+') == 3
     assert calculator.calculate(-4, -4, '+') == -8
     assert calculator.calculate(6.0, 1.5, '+') == 7.5
 
-def minus_test():
+def test_minus():
     assert calculator.calculate(1, 2, '-') == -1
     assert calculator.calculate(-4, -4, '-') == 0
     assert calculator.calculate(6.0, 1.5, '-') == 4.5
 
-def mult_test():
+def test_mult():
     assert calculator.calculate(1, 2, '*') == 2
     assert calculator.calculate(-4, -4, '*') == 16
     assert calculator.calculate(6.0, 1.5, '*') == 9
 
-def div_test():
+def test_div():
     assert calculator.calculate(1, 2, '/') == 0.5
     assert calculator.calculate(-4, -4, '/') == 1
     assert calculator.calculate(6.0, 1.5, '/') == 4
 
-def exp_test():
+def test_exp():
     assert calculator.calculate(1, 2, '^') == 1
     assert calculator.calculate(-4, 2, '^') == 16
     assert calculator.calculate(6.0, 1, '^') == 6
 
-def error_test():
+def test_error():
     assert calculator.calculate(1, 2, '$') == None
     assert calculator.calculate('abc', 2, '^') == None
     assert calculator.calculate('--1', 1, '-') == None
